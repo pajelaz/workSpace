287c63a54d5b5ddb76af5f216d83dda4d60b10dd
Pajelaz <zachary.pajela@gmail.com>
2022-09-22 20:46:43 -0500

    updated findMethod.py

--- findMethod.py
+++ findMethod.py
@@ -8,6 +8,7 @@ str = 'X-DSPAM-Confidence:0.8475'
 Use find and string slicing to extract the portion of the string after the colon character and then use the 
 float function to convert the extracted string into a floating point number.
 
+https://books.trinket.io/pfe/06-strings.html
 
 """
 
