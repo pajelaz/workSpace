07dd36dd9d8d0d9d885bf81c009ebde909291c5a
Pajelaz <zachary.pajela@gmail.com>
2022-09-22 23:49:55 -0500

    adding pandas stuff

---
+++ firstpan.py
@@ -0,0 +1,25 @@
+"""
+Zachary Pajela
+
+My first attempt at pandas
+
+wish me luck 
+
+
+"""
+
+import pandas as pd 
+
+maindf = pd.read_csv("income_threshold.csv")
+
+#this will print the first 5 records
+#print(maindf.head(), "\n")
+
+#this will print the last 5 records
+#print(maindf.tail())
+
+#this prints (num of rows, num of columns)
+#print(maindf.shape)
+
+
+print(maindf.dtypes)
