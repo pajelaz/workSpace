bf422a1c3f03ac06235770e4568b2a9acd8bf5c8
Zachary Pajela <zpajela@Zacharys-MacBook-Pro-2.local>
2022-09-22 23:46:30 -0500

    adding mac files

---
+++ ExerciseFive.py
@@ -0,0 +1,6 @@
+#Zachary Pajela
+#Exercise 5: Write a program which prompts the user for a Celsius temperature, 
+#convert the temperature to Fahrenheit, and print out the converted temperature.
+
+celTemp = input("Temperature in celsius: \n")
+print ("Temperature in Fahrenheit:", (float(celTemp)*1.8) +32)
