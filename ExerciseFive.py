20dada28f5298a231c272339c6a3753eee7130ec
Zachary Pajela <zpajela@Zacharys-MacBook-Pro-2.local>
2022-09-30 16:38:00 -0500

    Moving Files

--- ExerciseFive.py
+++
@@ -1,6 +0,0 @@
-#Zachary Pajela
-#Exercise 5: Write a program which prompts the user for a Celsius temperature, 
-#convert the temperature to Fahrenheit, and print out the converted temperature.
-
-celTemp = input("Temperature in celsius: \n")
-print ("Temperature in Fahrenheit:", (float(celTemp)*1.8) +32)
--- ExerciseTwo.py
+++
@@ -1,5 +0,0 @@
-# Zachary Pajela
-# Write a program that uses input to prompt a user for their name and then welcomes them.
-
-userName = input("Enter your name: \n")
-print("Hello and nice to see you,", userName + "!")
