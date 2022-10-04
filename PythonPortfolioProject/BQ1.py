21f86738c51812d02976f768434a9c31eaab5ba1
Zachary Pajela <zpajela@Zacharys-MacBook-Pro-2.local>
2022-10-04 16:50:27 -0500

    pandas project

---
+++ PythonPortfolioProject/BQ1.py
@@ -0,0 +1,17 @@
+"""
+Zachary Pajela
+
+Business Question: How can we create visuals that help represent the unequal pay woman have to men?
+
+
+
+
+
+"""
+
+import pandas as pd 
+import matplotlib as mb 
+#import numpy as np 
+
+inc = pd.read_csv('income_threshold.csv')
+print(inc.to_string())
